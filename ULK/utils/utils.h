0081672e4d6a401b1affeb05a95a93101296a50b
morgan <morgan@qq.com>
2025-06-18 16:00:34 +0800

    清除

--- ULK/utils/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ULK_COMMON_H
-#define _ULK_COMMON_H
-
-int create_syscall(unsigned long syscall_addr);
-int remove_syscall(int nr);
-
-#endif
--- kernel_projects/system_resources/src/sysinfo.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MEMINFO_H
-#define _MEMINFO_H
-
-#include <linux/mm.h>
-
-int do_sysinfo(struct sysinfo *si);
-
-#endif
